926c6835e95231667a9126ad755557a2e566e6c2
luxian <luxianaiolos@gmail.com>
2022-05-17 02:10:25 -0700

    Various Updates
    Fixed NavBar/dropdowns,
    Adjusted purchase tickets sign,
    adjusted footer and copyrights elements css,
    removed footer <li> elements,
    
    Most javascript was not running because of a useless function which has been commented out.
    
    Added css transform animation to icons on navbar,
    
    fixed email sign up functionality.

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const toggle = document.getElementById('toggle');
 const navtoggle = document.getElementById('nav-toggle')
-const email = document.getElementById('email');
+let email = ''
+const emailInput = document.getElementById('email').addEventListener('input', (e)=> {
+    email+= e.target.value
+})
+const submitButton = document.getElementById('signup')
 
 
-// Toggle Nav Bar   
-toggle.addEventListener('click', () =>
-    navtoggle.classList.toggle('show-toggle')
-);
 
+// // Toggle Nav Bar   
+// toggle.addEventListener('click', () =>
+//     navtoggle.classList.toggle('show-toggle')
+// );
+//had to comment out the part above, it was keeping the rest of the code from running.
 
 
 // Slide Show
@@ -23,7 +28,6 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-
     var i;
     var slides=document.getElementsByClassName("mySlides");
     var dots=document.getElementsByClassName("dot");
@@ -40,7 +44,7 @@ function showSlides(n) {
         dots[i].className=dots[i].className.replace("active", "");
     }
 
-    slides[slideIndex-1].style.display="block";
+    slides[slideIndex-1].style.display="block";``
 
     dots[slideIndex-1].className+=" active";
 }
@@ -48,9 +52,7 @@ function showSlides(n) {
 var timer = setInterval(function(){
     slideIndex++;
     showSlides(slideIndex);
-},2000);
-
-
+},3000);
 
 
 // Email REGEX check function
@@ -58,15 +60,17 @@ function isValidEmail(email) {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     //const re uses the expression above
     if(re.test(String(email).toLowerCase())) {
-        showSuccess(input);
+        // showSuccess(input);
+        alert("Thanks for Signing Up!")
 } else {
     alert("Email Not Valid");
 }
 }
 
 // event listeners
-email.addEventListener('submit', function(e) {
+submitButton.addEventListener('click', function(e) {
+    console.log(email)
     e.preventDefault()
-
     isValidEmail(email);
+    email = ''
 });
